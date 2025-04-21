338820a32aeb6c43b84d32a5c3e2ced85cf4e597
Tim Murphy <tim@murphy.org>
2025-04-22 09:58:47 +1000

    code cleanup

--- import_raw.py
+++ import_raw.py
@@ -1,10 +1,24 @@
-import pyreadstat
+'''
+Script to batch convert a directory of SAS files (sas7bdat extension) to
+SPSS files (sav extension).
+
+Written by Tim Murphy <tim.murphy@canberra.edu.au> 2025
+'''
+
 import argparse
 import glob
 import os
 import sys
 
+import pyreadstat
+
 def confirm_yes_no(text):
+    '''
+    Prompt the user to answer y or n to a question.
+    @return True if the answer is y, or False otherwise.
+    @param text The text displayed to the user.
+    '''
+
     response = None
 
     while response not in ("y", "n"):
@@ -18,32 +32,35 @@ def confirm_yes_no(text):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", type=str, required=True, help="Directory containing sas7bdat files")
-    parser.add_argument("--output_dir", type=str, required=True, help="Directory to save converted files")
+    parser.add_argument("--input_dir", type=str, required=True,
+                        help="Directory containing sas7bdat files")
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="Directory to save converted files")
     args = parser.parse_args()
 
     # Error checking.
-    bad_args = False
+    bad_args: bool = False
 
     for (dir_path, can_create) in ((args.input_dir, False), (args.output_dir, True)):
         if not os.path.isdir(dir_path):
             if not can_create:
                 print("ERROR: directory does not exist: '" + dir_path + "'", file=sys.stderr)
-                bad_args = True
+                bad_args = True # pylint: disable=invalid-name
             else:
-                if confirm_yes_no("Directory does not exixt: '" + dir_path + "'! Do you want to create it?"):
+                if confirm_yes_no("Directory does not exixt: '" + dir_path +\
+                                  "'! Do you want to create it?"):
                     print("Creating directory...", end="", flush=True)
                     os.makedirs(dir_path)
                     print("done")
                 else:
                     print("ERROR: not creating directory.", file=sys.stderr)
-                    bad_args = True
+                    bad_args = True # pylint: disable=invalid-name
 
     if bad_args:
         print("An error occurred while parsing the command line arguments. Goodbye.")
         sys.exit(1)
 
-    files_copied = 0
+    files_copied: int = 0
     failures = []   # (filename, error)
     for sas in glob.glob(os.path.join(args.input_dir, "*.sas7bdat")):
         file_root = os.path.splitext(os.path.split(sas)[1])[0]
@@ -64,12 +81,12 @@ if __name__ == '__main__':
                     cols_clean.append("v" + col)
                 else:
                     cols_clean.append(col)
-            df.columns = cols_clean
+            # df.columns = cols_clean
 
             pyreadstat.write_sav(df, outfile, file_label=file_root)
             files_copied += 1
             print("done")
-        except pyreadstat._readstat_parser.PyreadstatError as e:
+        except Exception as e: # pylint: disable=broad-exception-caught
             failures.append((file_root, str(e)))
             print("!!! FAILED !!!")
 
