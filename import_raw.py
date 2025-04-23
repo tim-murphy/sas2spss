eb876c7b472f2af69a39d74fad33178c6aac9e2d
Tim Murphy <tim@murphy.org>
2025-04-24 09:04:53 +1000

    Allow files to be merged (outer join)

--- import_raw.py
+++ import_raw.py
@@ -37,9 +37,11 @@ if __name__ == '__main__':
     parser.add_argument("--output_dir", type=str, required=True,
                         help="Directory to save converted files")
     parser.add_argument("--single_file", type=str, required=False,
-                        help="If set, will also (outer) merge into one giant file with this filename")
-    parser.add_argument("--merge_key", type=str, default="PID",
+                        help="If set, will also (outer) merge into one file with this filename")
+    parser.add_argument("--merge_keys", default=["ID"], nargs="+",
                         help="If merging, use this as the merge key")
+    parser.add_argument("--prefix_vars", default=True, action="store_true",
+                        help="Add the filename as a prefix to variables")
     args = parser.parse_args()
 
     # Error checking.
@@ -66,31 +68,69 @@ if __name__ == '__main__':
 
     files_copied: int = 0
     failures = []   # (filename, error)
-    merged = None
-    for sas in glob.glob(os.path.join(args.input_dir, "*.sas7bdat")):
+    merge_failures = []   # (filename, error)
+    merged = None # pylint: disable=invalid-name
+
+    datfiles = glob.glob(os.path.join(args.input_dir, "*.sas7bdat"))
+    for i, sas in enumerate(datfiles):
+        print("[", i+1, " / ", len(datfiles), "] ", sep="", end="", flush=True)
+
         file_root = os.path.splitext(os.path.split(sas)[1])[0]
         outfile = os.path.join(args.output_dir, file_root + ".sav")
 
+        skip: bool = False
         if os.path.exists(outfile):
             print("Skipping file as it already exists:", outfile)
+            skip = True # pylint: disable=invalid-name
+        else:
+            print("Copying ", sas, " to ", outfile, "...", sep="", end="", flush=True)
+
+        if skip and args.single_file is None:
             continue
 
-        print("Copying ", sas, " to ", outfile, "...", sep="", end="", flush=True)
         try:
             df, _ = pyreadstat.read_file_multiprocessing(pyreadstat.read_sas7bdat, sas)
 
             # Some columns start with an underscore, which is not allowed by write_sav.
             cols_clean = []
             for col in df.columns:
+                clean = col
+
+                # Variables cannot start with an underscore.
                 if col[0] == "_":
-                    cols_clean.append("v" + col)
-                else:
-                    cols_clean.append(col)
+                    clean = "v" + col
+
+                # Data cleaning: merge keys are case sensitive.
+                iskey: bool = False
+                for key in args.merge_keys:
+                    if clean.casefold() == key.casefold():
+                        clean = key
+                        iskey = True # pylint: disable=invalid-name
+
+                # Some variable names may be duplicated between files.
+                if args.prefix_vars and not iskey:
+                    clean = file_root + "_" + clean
+
+                cols_clean.append(clean)
+
             df.columns = cols_clean
 
-            pyreadstat.write_sav(df, outfile, file_label=file_root)
-            files_copied += 1
-            print("done")
+            # Merge the dataframes.
+            if args.single_file is not None:
+                if merged is None:
+                    merged = df.copy(deep=True)
+                else:
+                    try:
+                        merged = merged.merge(df, how="outer", on=args.merge_keys,
+                                              validate="one_to_one", sort=True)
+                    except Exception as e: # pylint: disable=broad-exception-caught
+                        merge_failures.append((file_root, str(e)))
+                        print("!!! FAILED TO MERGE !!!")
+
+            if not skip:
+                pyreadstat.write_sav(df, outfile, file_label=file_root)
+                files_copied += 1
+                print("done")
         except Exception as e: # pylint: disable=broad-exception-caught
             failures.append((file_root, str(e)))
             print("!!! FAILED !!!")
@@ -102,6 +142,26 @@ if __name__ == '__main__':
         print("The following files could not be copied:")
         for (path, err) in failures:
             print("   ", path, "::", err)
+        print()
+
+    # Write the merged file to disk. This may take some time...
+    if merged is not None:
+        try:
+            merged_outfile = os.path.join(args.output_dir, args.single_file)
+            print("Writing merged file to ", merged_outfile, "...", end="", sep="", flush=True)
+            pyreadstat.write_sav(merged, merged_outfile)
+            print("done")
+        except Exception as e: # pylint: disable=broad-exception-caught
+            print()
+            print("Error creating merged file: " + str(e), file=sys.stderr)
+
+        print()
+
+    if len(merge_failures) > 0:
+        print("The following files could not be merged:")
+        for (path, err) in merge_failures:
+            print("   ", path, "::", err)
+        print()
 
     print("All done! Have a nice day :)")
 
